9017cc542510eca57a2eab4cfb61f50ac843a968
System Administrator <root@ASEPs-MacBook-Pro.local>
2020-05-29 00:28:44 +0700

    menambahkan file coba.php

---
+++ BELAJAR/Coba.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="1">
+    <label>
+      Username :
+      <input type="text" name="username">
+    </label>
+    <label>
+      Nama :
+      <input type="text" name="nama">
+    </label>
+    <label>
+      NIM :
+      <input type="text" name="nim">
+    </label>
+  </form>
+</body>
+
+</html>
